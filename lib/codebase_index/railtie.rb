f44459fff8b29f59a0473eb2d5dc8ac7c69c0ab8
Leah Armstrong <info@leah.wtf>
2026-02-09 23:22:43 -0500

    Add Railtie to auto-load rake tasks into Rails apps
    Without a Railtie, the rake tasks in lib/tasks/ were invisible to host
    Rails apps. The Railtie loads them automatically when the gem is bundled.

---
+++ lib/codebase_index/railtie.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module CodebaseIndex
+  # Railtie integrates CodebaseIndex into Rails applications.
+  # Loads rake tasks automatically when the gem is bundled.
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      load File.expand_path("../tasks/codebase_index.rake", __dir__)
+    end
+  end
+end
